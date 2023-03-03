175b642804be4a9e8ccc4f922261d50982e9b82f
zuleika <njokizuleika@gmail.com>
2023-03-03 15:24:07 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+// Your code herefunction moveDodgerLeft() {
+    function moveDodgerLeft() {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+      
+        if (left > 0) {
+          dodger.style.left = `${left - 1}px`;
+        }
+      } document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowLeft") {
+          moveDodgerLeft();
+        }
+      }); 
+      function moveDodgerRight() {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+      
+        if (left > 0) {
+          dodger.style.left = `${left + 1}px`;
+        }
+      } document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowLeft") {
+          moveDodgerRight();
+        }
+      });
